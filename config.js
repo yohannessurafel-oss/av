2d376b04fea04be038a04328e15567eb7de460e9
yohannessurafel-oss <yohannes.surafel@gmail.com>
2026-04-08 14:23:23 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 // config.js
 const supabaseUrl = 'https://oxzthrubidohuwwhxsrk.supabase.co';
 const supabaseKey = 'sb_publishable_ORZuRj6_fb0rMSYDsd1raw_GoMNuYPS';
-
 // We attach it to 'window' so it becomes a global variable
 // that your other scripts can see.
 if (typeof supabase !== 'undefined') {
