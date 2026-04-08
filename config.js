71b23753aa01ee0731a4865ebedf0860791b918e
yohannessurafel-oss <yohannes.surafel@gmail.com>
2026-04-08 11:08:46 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+
+// Ensure you have ' ' around the URL and Key!
+const supabaseUrl = 'https://oxzthrubidohuwwhxsrk.supabase.co';  //data api
+const supabaseKey = 'ssb_publishable_ORZuRj6_fb0rMSYDsd1raw_GoMNuYPS'; // Api key quotes here!
