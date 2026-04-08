072061a0efa6bfd88c282719e1ad4405d627a93e
yohannessurafel-oss <yohannes.surafel@gmail.com>
2026-04-08 13:19:20 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,3 +2,5 @@
 // Ensure you have ' ' around the URL and Key!
 const supabaseUrl = 'https://oxzthrubidohuwwhxsrk.supabase.co';  //data api
 const supabaseKey = 'ssb_publishable_ORZuRj6_fb0rMSYDsd1raw_GoMNuYPS'; // Api key quotes here!
+
+window._supabase = supabase.createClient(supabaseUrl, supabaseKey);
