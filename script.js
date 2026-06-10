f174ff4045222c19a4fd4e5454212daf9b446a10
Avmf <yohannes.surafel@gmail.com>
2026-06-10 14:23:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = 'YOUR_PROJECT_URL';
-const SUPABASE_KEY = 'YOUR_ANON_KEY';
+const SUPABASE_URL = 'https://oxzthrubidohuwwhxsrk.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94enRocnViaWRvaHV3d2h4c3JrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2MzExMTIsImV4cCI6MjA5MTIwNzExMn0.6NrwYlDDVzYZNouknbdPGtvNb_0GLkT12T370fyPRyA';
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 async function fetchLedger() {
