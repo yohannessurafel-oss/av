85033c5f25befdfe741321e0df5811096ca0599a
Avmf <yohannes.surafel@gmail.com>
2026-07-15 15:24:41 +0300

    Update loan-repayment-posting.js

--- loan-repayment-posting.js
+++ loan-repayment-posting.js
@@ -9,7 +9,7 @@
    ============================================================================ */
 
 const SUPABASE_URL = 'https://oxzthrubidohuwwhxsrk.supabase.co';
-const SUPABASE_ANON_KEY = 'REPLACE_WITH_YOUR_ANON_KEY';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94enRocnViaWRvaHV3d2h4c3JrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2MzExMTIsImV4cCI6MjA5MTIwNzExMn0.6NrwYlDDVzYZNouknbdPGtvNb_0GLkT12T370fyPRyA';
 
 async function sbFetch(path, opts = {}) {
   const res = await fetch(`${SUPABASE_URL}/rest/v1/${path}`, {
